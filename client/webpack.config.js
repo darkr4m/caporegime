18ad4a8ef9f31016ca66252aeb40b8b96b488722
max <matsumouri@gmail.com>
2019-12-27 12:53:30 -0600

    Installed and configured webpack in dev dependencies for react.

---
+++ client/webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: index_bundle.js
+    },
+    modules: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            }
+        ]
+    }
+
+}
