0338be8c5bea68bcba083333316553a685bc37e7
max <matsumouri@gmail.com>
2019-12-30 13:34:56 -0600

    Initialized connection to database in server.js.

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 const PORT = process.env.PORT || 5000; //Development port.
 
+//Connect to the Database
+connectDB();
 
 //Sample route
 app.get('/', (req,res) => res.send('API Running.'));
