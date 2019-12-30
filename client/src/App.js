bfcbeea1b2822b7575b8c2e3ecbfee2d8fda27a8
max <matsumouri@gmail.com>
2019-12-30 12:20:48 -0600

    Corrected location of App.js to src folder instead of components.
    Updated all references.

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import React, { Component, Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 // Components
-import Navbar from './layout/Navbar';
-import Landing from './layout/Landing';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 
 class App extends Component {
     render(){
